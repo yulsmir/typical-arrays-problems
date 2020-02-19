a0900aae9781ecdc5f57ac3cf5e738059c0db010
julia <julianna.smirnova@gmail.com>
2020-02-19 19:26:32 +0200

    init: task solved

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+    return array === undefined || array.length === 0 ? 0 : Math.min(...array);
+};
 
 exports.max = function max (array) {
-  return 0;
-}
+    return array === undefined || array.length === 0 ? 0 : Math.max(...array);
+};
 
 exports.avg = function avg (array) {
-  return 0;
-}
+    return array === undefined || array.length === 0 ? 0 : array.reduce((a, b) => a + b)/array.length;
+};
